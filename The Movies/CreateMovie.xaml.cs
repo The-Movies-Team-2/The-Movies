147fe9a67b83717d4437ac037a5d47d937c8d05e
Ea Overvad <ea@0vervad.dk>
2024-08-15 14:41:16 +0200

    vidensdeling

--- The Movies/CreateMovie.xaml.cs
+++ The Movies/CreateMovie.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +23,12 @@ namespace The_Movies
         public CreateMovieWindow()
         {
             InitializeComponent();
+        }
+        private void NumericTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            //regular expression(regex) genkender mønstrer og sørger for der kun kan indtastes tal i textboksen
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
