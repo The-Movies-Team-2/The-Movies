627826658cb51dc731a2956b112143ee99120329
alexs <alex.salling@hotmail.com>
2024-08-20 10:13:21 +0200

    Working with UI

---
+++ The Movies/Model/Cinema.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Movies.Model
+{ 
+    internal class Cinema
+    {
+        public enum Cinemas
+        {
+            Hjerm,
+            Videbaek,
+            Thorsminde,
+            Raehr,
+
+        }
+
+        public enum HjermSale
+        
+        {
+            H1,
+            H2, 
+            H3,
+        }
+
+        public enum VidebaekSale
+        
+        {
+            V1,
+            V2, 
+            V3,
+            V4,
+        }
+
+        public enum ThorsmindeSale
+        
+        {
+            T1,
+            T2,
+        }
+
+        public enum RaehrSale
+        
+        {
+            R1,
+            R2,
+            R3,
+        }
+    }
+}
--- The Movies/Model/Movie.cs
+++ The Movies/Model/Movie.cs
@@ -8,17 +8,33 @@ namespace The_Movies.Model
 {
     internal class Movie
     {
-        public int Id;
-        public string Title { get; set; }
-        public int PlayingTime { get; set; }
-        public List<int> genreids = new List<int>();
+        private int _id;
+        public int Id { get; set; }
 
-        public Movie(string title, int id, int time, List<int> genres)
+        private string _title;
+        public string Title { get; set; }
+
+        private int _PlayingTime;
+        public int PlayingTime { get; set; }
+
+        private List<int> _genreids = new List<int>();
+        public List<int> genreids { get; set; }
+
+        private string _Director;
+        public string Director { get; set; }
+
+        private float _PremierDate;
+        public float PremierDate { get; set; }
+        
+        public Movie(string title, int id, int time, string director, List<int> genres, float date)
         {
             Title = title;
             PlayingTime = time;
             Id = id;
             genreids = genres;
+            Director = director;
+            PremierDate = date;
+            
         }
 
         public Movie(string title, int time, List<int> genres)
